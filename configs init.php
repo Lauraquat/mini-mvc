af3ad760f5adbc2f0e7b7c56a13bfb6311f5a699
Lauraquat <lauraquat@gmail.com>
2023-01-06 10:55:41 +0100

    ajout configs init.php

---
+++ configs init.php
@@ -0,0 +1,16 @@
+<?php
+
+$DB_SERVER = getenv("MVC_SERVER") ?: "127.0.0.1";
+$DB_DATABASE = getenv("MVC_DB") ?: "";
+$DB_USER = getenv("MVC_USER") ?: "";
+$DB_PASSWORD = getenv("MVC_TOKEN") ?: "";
+$DEBUG = getenv("MVC_DEBUG") ?: true;
+
+return array(
+    "DB_USER" => $DB_USER,
+    "DB_PASSWORD" => $DB_PASSWORD,
+    "DB_DSN" => "mysql:host=$DB_SERVER;dbname=$DB_DATABASE;charset=utf8",
+    "DEBUG" => $DEBUG
+);
+
+//A copier et renommer "configs.php" et remplir les $DB_DATABASE, $DB_USER, $DB_PASSWORD
