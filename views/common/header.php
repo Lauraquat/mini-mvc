394af4febf2938130a006d95ec356985606f409b
lea <lea.sagnier@gmail.com>
2023-01-30 18:09:43 +0100

    delete bootstrap

--- views/common/header.php
+++ views/common/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Retour vers le futur</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="/public/style/main.css">
     <script src="/public/js/script.js"></script>
 
@@ -15,8 +15,15 @@
     <a class="lienlogo" href="/"> 
         <img src="/public/image/logo_rvf.svg" alt="logo site">
     </a>
-    <div id="menu-button" class="close" onclick="openmenu()"></div>
-    <div id="menu-button2" class="" onclick="closemenu()"></div>
+    <div class="div-menu-connect">
+        <?php if(!isset($_SESSION["username"])):?>
+            <a href="/login">Se connecter</a>
+            <!-- <a href="/register">S'inscrire</a> -->
+            <a href="/logout">Déconnexion</a>
+        <?php endif; ?>
+        <div id="menu-button" class="close" onclick="openmenu()"></div>
+        <div id="menu-button2" class="" onclick="closemenu()"></div>
+    </div>
 
     <nav id="menu" class="">
         <ul>
--- views/global/home.php
+++ views/global/home.php
@@ -3,18 +3,10 @@
 session_start();
 ?>
 
-<?php if(!isset($_SESSION["username"])):?>
-    <a href="/login">Se connecter</a>
-    <a href="/register">S'inscrire</a>
-<?php endif; ?>
-
 
 <?php if(isset($_SESSION["username"])):?>
-    <div class="sucess">
-    <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
-    <p>Voici notre site sur la saga Retour vers le Futur</p>
-    <a href="/logout">Déconnexion</a>
+    <div class="">
+        <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
+        <p>Voici notre site sur la saga Retour vers le Futur</p>
     </div>
 <?php endif; ?>
-
-<?php require ('./common/footer.php') ?>
